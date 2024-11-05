257625df6e3406fc7e64141bc4ea97db1c38b97a
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-05 21:27:58 +0000

    auto commit 4       20241105 212751

--- src/workflows/502_run_workflow_vivencial_001.r
+++ src/workflows/502_run_workflow_vivencial_001.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/527_workflow_base_f202107_viv.r"
+PARAM <- "src/workflows/527_workflow_base_f202107_viv_002.r"
 
 envg <- env()
 
