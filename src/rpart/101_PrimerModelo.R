8297578af99b4f4359653c9a91b6caad1f2b9d72
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-18 23:40:47 +0000

    auto commit 28       20241118 234037

--- src/rpart/101_PrimerModelo.R
+++ src/rpart/101_PrimerModelo.R
@@ -22,10 +22,10 @@ modelo <- rpart(
     formula = "clase_ternaria ~ .",
     data = dtrain, # los datos donde voy a entrenar
     xval = 0,
-    cp = -0.7852951302, # esto significa no limitar la complejidad de los splits
-    minsplit = 2175, # minima cantidad de registros para que se haga el split
-    minbucket = 434, # tamaño minimo de una hoja
-    maxdepth = 7  # profundidad maxima del arbol
+    cp = -0.8587956495, # esto significa no limitar la complejidad de los splits
+    minsplit = 5190, # minima cantidad de registros para que se haga el split
+    minbucket = 10, # tamaño minimo de una hoja
+    maxdepth = 3  # profundidad maxima del arbol
 )
 
 
@@ -61,6 +61,6 @@ dir.create("./exp/KA2001")
 
 # solo los campos para Kaggle
 fwrite(dapply[, list(numero_de_cliente, Predicted)],
-        file = "./exp/KA2001/K101_rf_001_viv.csv",
+        file = "./exp/KA2001/K101_rf_050_viv.csv",
         sep = ","
 )
