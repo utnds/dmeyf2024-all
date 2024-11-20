060627c8b6b3a97eb98c8813b5017e554950d133
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-20 23:35:48 +0000

    auto commit 33       20241120 233537

--- src/workflows/z990_workflow_orden227_SEMI.r
+++ src/workflows/z990_workflow_orden227_SEMI.r
@@ -465,9 +465,11 @@ wf_SEMI_sep_orden227 <- function( pnombrewf )
 
   DT_incorporar_dataset_competencia2024()
 
-  CA_catastrophe_base( metodo="MachineLearning")
+ # CA_catastrophe_base( metodo="MachineLearning")
+  CA_catastrophe_base( metodo="Ninguno")
   FEintra_manual_base()
-  DR_drifting_base(metodo="rank_cero_fijo")
+ # DR_drifting_base(metodo="rank_cero_fijo")
+  DR_drifting_base(metodo="deflacion")
   FEhist_base()
   ultimo <- FErf_attributes_base()
   #CN_canaritos_asesinos_base(ratio=0.2, desvio=4.0)
