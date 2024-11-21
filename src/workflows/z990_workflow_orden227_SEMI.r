36482aab393fa5d3574a7f00b704a90fa25001ce
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-21 02:15:09 +0000

    auto commit 34       20241121 021458

--- src/workflows/z990_workflow_orden227_SEMI.r
+++ src/workflows/z990_workflow_orden227_SEMI.r
@@ -278,7 +278,9 @@ TS_strategy_base9 <- function( pinputexps )
   param_local$final_train$clase_minoritaria <- c( "BAJA+1", "BAJA+2")
   param_local$final_train$training <- c(
     202107, 202106, 202105, 202104, 202103, 202102, 202101, 
-    202012, 202011, 202010, 202009, 202008, 202007, 
+    202012, 202011, 
+    #202010, 202009, 202008, Excluyo por pandemia
+    202007, 
     # 202006  Excluyo por variables rotas
     202005, 202004, 202003, 202002, 202001,
     201912, 201911,
@@ -294,7 +296,9 @@ TS_strategy_base9 <- function( pinputexps )
 
   param_local$train$training <- c(
     202105, 202104, 202103, 202102, 202101, 
-    202012, 202011, 202010, 202009, 202008, 202007, 
+    202012, 202011, 
+    #202010, 202009, 202008, Excluyo por pandemia
+    202007, 
     # 202006  Excluyo por variables rotas
     202005, 202004, 202003, 202002, 202001,
     201912, 201911,
