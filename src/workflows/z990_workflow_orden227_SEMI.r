a396396fe33edd0ef92b104b4aa181e975da04f0
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-22 23:03:15 +0000

    auto commit 38       20241122 230302

--- src/workflows/z990_workflow_orden227_SEMI.r
+++ src/workflows/z990_workflow_orden227_SEMI.r
@@ -476,8 +476,8 @@ wf_SEMI_sep_orden227 <- function( pnombrewf )
   DR_drifting_base(metodo="deflacion")
   FEhist_base()
   ultimo <- FErf_attributes_base()
-  #CN_canaritos_asesinos_base(ratio=0.2, desvio=4.0)
-  CN_canaritos_asesinos_base(ratio=1, desvio=1)
+  CN_canaritos_asesinos_base(ratio=0.2, desvio=4.0)
+  #CN_canaritos_asesinos_base(ratio=1, desvio=1)
 
   ts9 <- TS_strategy_base9()
 
