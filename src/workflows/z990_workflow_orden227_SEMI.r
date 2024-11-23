6d835f1affa91d28b51a83372d47ab1db3023296
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-23 18:27:11 +0000

    auto commit 40       20241123 182701

--- src/workflows/z990_workflow_orden227_SEMI.r
+++ src/workflows/z990_workflow_orden227_SEMI.r
@@ -469,16 +469,16 @@ wf_SEMI_sep_orden227 <- function( pnombrewf )
 
   DT_incorporar_dataset_competencia2024()
 
-  CA_catastrophe_base( metodo="MachineLearning")
- # CA_catastrophe_base( metodo="Ninguno")
+ #CA_catastrophe_base( metodo="MachineLearning")
+ CA_catastrophe_base( metodo="Ninguno")
   FEintra_manual_base()
   # DR_drifting_base(metodo="rank_cero_fijo")
-  # DR_drifting_base(metodo="deflacion")
-   DR_drifting_base(metodo="UVA")
+   DR_drifting_base(metodo="deflacion")
+  # DR_drifting_base(metodo="UVA")
   FEhist_base()
   ultimo <- FErf_attributes_base()
   #CN_canaritos_asesinos_base(ratio=0.2, desvio=4.0)
-  CN_canaritos_asesinos_base(ratio=1, desvio=4)
+  CN_canaritos_asesinos_base(ratio=0.2, desvio=1)
 
   ts9 <- TS_strategy_base9()
 
@@ -493,7 +493,7 @@ wf_SEMI_sep_orden227 <- function( pnombrewf )
     c(ht, ts9), # los inputs
     ranks = c(1), # 1 = el mejor de la bayesian optimization
     semillerio = 50,   # cantidad de semillas finales
-    repeticiones_exp = 1  # cantidad de repeticiones del semillerio
+    repeticiones_exp = 2  # cantidad de repeticiones del semillerio
   )
 
   SC_scoring_semillerio( c(fm, ts9) )
