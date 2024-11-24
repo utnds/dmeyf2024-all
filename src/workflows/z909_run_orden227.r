f09e12d95b6e1db4dc7ed7beaea64b801db679ca
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-11-24 13:49:16 +0000

    auto commit 3       20241124 134909

--- src/workflows/z909_run_orden227.r
+++ src/workflows/z909_run_orden227.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/finalfinal_990_workflow_orden227_SEMI.r"
+PARAM <- "src/workflows/finalfinal1_990_workflow_orden227_SEMI.r"
 
 envg <- env()
 
