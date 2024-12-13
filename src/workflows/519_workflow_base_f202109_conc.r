1ab58918d654e473b7382abee040d60c4cd44bee
GustavoDenicolay <gustavo.denicolay@gmail.com>
2024-12-13 05:31:06 +0000

    auto commit 7       20241213 053054

--- src/workflows/519_workflow_base_f202109_conc.r
+++ src/workflows/519_workflow_base_f202109_conc.r
@@ -432,9 +432,9 @@ wf_septiembre <- function( pnombrewf )
   param_local <- exp_wf_init( pnombrewf ) # linea fija
 
   DT_incorporar_dataset_competencia2024()
-  CA_catastrophe_base( metodo="MachineLearning")
+  CA_catastrophe_base( metodo="Ninguno")
   FEintra_manual_base()
-  DR_drifting_base(metodo="rank_cero_fijo")
+  DR_drifting_base(metodo="deflacion")
   FEhist_base()
 
   FErf_attributes_base( arbolitos= 20,
